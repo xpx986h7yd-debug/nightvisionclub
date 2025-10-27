74f7488ba61b2922bf8e2a9cc53cf553fd677074
xpx986h7yd-debug <xpx986h7yd@privaterelay.appleid.com>
2025-10-27 10:35:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
