ef4f26461b10fe5f5c2d62e1e1fc357562c706b6
xpx986h7yd-debug <xpx986h7yd@privaterelay.appleid.com>
2025-10-27 10:53:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
-import './globals.css';
+// app/layout.tsx
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "NightVision",
+  description: "Nouveau chat pour le site NightVision",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
