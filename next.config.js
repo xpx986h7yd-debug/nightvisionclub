cf189b91cea3de66b21be1b74216468ce8783be9
xpx986h7yd-debug <xpx986h7yd@privaterelay.appleid.com>
2025-10-27 10:47:40 +0100

    Configure ESLint and TypeScript settings
    Added ESLint and TypeScript configuration options.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false } // laisse à false pour garder la sécurité
+};
 module.exports = nextConfig;
